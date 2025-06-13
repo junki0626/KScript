0f13701d0d250104cd3c4ce06b1a6911f290c696
junki0626 <158823476+junki0626@users.noreply.github.com>
2025-06-13 22:38:30 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function run(code){
+    const cmd = code[0]
+    if(cmd == "log"){
+        console.log(code[1]);
+    }
+    if(cmd == "while"){
+        while(code[1] != code[2]){
+            if(code[1] == code[2]){
+                break;
+            }
+            run(code[3]);
+            code[1]++;
+        }
+    }
+    if (cmd == "if") {
+        if(code[1] == code[2]){
+            run(code[3]);
+        }
+    }
+
+}
+
+const file = process.argv[2];
+const raw = fs.readFileSync(file, 'utf-8');
+const code = JSON.parse(raw);
+run(code);
